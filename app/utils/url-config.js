555eeb5dd921a11add7c70696a5a0069b0959f7d
Alexander Danilov <git@modos189.ru>
2025-05-07 15:05:46 +0500

    feat: Add support for dynamic internal hostnames management
    Add app.addInternalHostname() command and align hostname validation with Java implementation to properly handle exact matches and subdomains

--- app/utils/url-config.js
+++ app/utils/url-config.js
@@ -1,6 +1,6 @@
 export const INGRESS_INTEL_MAP = 'https://intel.ingress.com/';
 
-export const WEBVIEW_ALLOWED_DOMAINS = [
+export const INITIAL_INTERNAL_HOSTNAMES = [
   'intel.ingress.com',
   'signin.nianticlabs.com'
 ];
