d15d3bd8b5c4a1f65c534eed7994a91228e9521b
Alexander Danilov <git@modos189.ru>
2025-05-07 15:06:28 +0500

    fix: Add `signin.nianticspatial.com` to allowed hostnames

--- app/utils/url-config.js
+++ app/utils/url-config.js
@@ -2,5 +2,6 @@ export const INGRESS_INTEL_MAP = 'https://intel.ingress.com/';
 
 export const INITIAL_INTERNAL_HOSTNAMES = [
   'intel.ingress.com',
-  'signin.nianticlabs.com'
+  'signin.nianticlabs.com',
+  'signin.nianticspatial.com'
 ];
