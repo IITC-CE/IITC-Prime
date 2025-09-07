4c1f29d5569db582e361c6ad465068d937ba6961
Alexander Danilov <git@modos189.ru>
2025-09-07 13:12:49 +0500

    fix(ios): enable popup support for Google Auth

--- app/utils/webview/base-chrome-client.js
+++ app/utils/webview/base-chrome-client.js
@@ -4,60 +4,65 @@ import { isAndroid } from "@nativescript/core";
 import { transportManager } from "./transport-manager";
 
 /**
- * WebView chrome client implementation
+ * WebView Chrome Client for Android popup handling
+ * iOS popup handling is done natively by ui-webview patch
  */
-export const BaseWebChromeClient = isAndroid ? android.webkit.WebChromeClient.extend({
-  init: function () {},
+export const BaseWebChromeClient = isAndroid
+  ? android.webkit.WebChromeClient.extend({
+      init: function () {},
 
-  initWithComponent: function(component) {
-    this.component = component;
-  },
+      initWithComponent: function (component) {
+        this.component = component;
+      },
 
-  onProgressChanged: function(view, progress) {
-    if (this.component?.setProgress) {
-      this.component.setProgress(progress);
-    }
-  },
+      onProgressChanged: function (view, progress) {
+        if (this.component?.setProgress) {
+          this.component.setProgress(progress);
+        }
+      },
 
-  onConsoleMessage: function(consoleMessage) {
-    console.log(`[WebView] ${consoleMessage.message()} -- From line ${consoleMessage.lineNumber()} of ${consoleMessage.sourceId()}`);
-    return true;
-  },
+      onConsoleMessage: function (consoleMessage) {
+        console.log(
+          `[WebView] ${consoleMessage.message()} -- From line ${consoleMessage.lineNumber()} of ${consoleMessage.sourceId()}`
+        );
+        return true;
+      },
 
-  onJsAlert: function(view, url, message, result) {
-    console.log(`[WebView Alert] ${message}`);
-    result.confirm();
-    return true;
-  },
+      onJsAlert: function (view, url, message, result) {
+        console.log(`[WebView Alert] ${message}`);
+        result.confirm();
+        return true;
+      },
 
-  onCreateWindow: function(view, isDialog, isUserGesture, resultMsg) {
-    if (!isUserGesture) return false;
+      onCreateWindow: function (view, isDialog, isUserGesture, resultMsg) {
+        if (!isUserGesture) return false;
 
-    if (this.component?.showPopup) {
-      const popupData = transportManager.storeTransport(resultMsg);
-      this.component.showPopup(popupData);
-      return true;
-    }
-    return false;
-  },
+        if (this.component?.showPopup) {
+          const popupData = transportManager.storeTransport(resultMsg);
+          this.component.showPopup(popupData);
+          return true;
+        }
+        return false;
+      },
 
-  onCloseWindow: function(window) {
-    if (this.component?.closePopup) {
-      this.component.closePopup();
-    }
-  },
+      onCloseWindow: function (window) {
+        if (this.component?.closePopup) {
+          this.component.closePopup();
+        }
+      },
 
-  cleanup: function() {
-    this.component = null;
-  }
-}) : class BaseWebChromeClient {
-  init() {}
+      cleanup: function () {
+        this.component = null;
+      },
+    })
+  : class EmptyWebChromeClient {
+      init() {}
 
-  initWithComponent(component) {
-    this.component = component;
-  }
+      initWithComponent(component) {
+        this.component = component;
+      }
 
-  cleanup() {
-    this.component = null;
-  }
-};
+      cleanup() {
+        this.component = null;
+      }
+    };
--- app/utils/webview/webview-settings.js
+++ app/utils/webview/webview-settings.js
@@ -1,6 +1,6 @@
 // Copyright (C) 2024 IITC-CE - GPL-3.0 with Store Exception - see LICENSE and COPYING.STORE
 
-import { isAndroid } from "@nativescript/core";
+import { isAndroid, isIOS } from "@nativescript/core";
 import { sanitizeUserAgent, getDesktopUserAgent } from "~/utils/webview/user-agent";
 
 // Store original user agent to avoid losing it when switching modes
@@ -37,6 +37,59 @@ export function applyWebViewSettings(webview, fakeUserAgent = false) {
     return {
       originalUserAgent: cachedOriginalUserAgent,
       finalUserAgent
+    };
+  } else if (isIOS) {
+    // iOS WKWebView settings
+    const webview_ios = webview.ios;
+
+    if (webview_ios && webview_ios.configuration) {
+      const configuration = webview_ios.configuration;
+      const preferences = configuration.preferences;
+
+      // Enable JavaScript popup windows for iOS popup support
+      preferences.javaScriptCanOpenWindowsAutomatically = true;
+
+      // Set required cookie for Niantic auth (from IITC-Mobile)
+      try {
+        const cookieStore = configuration.websiteDataStore.httpCookieStore;
+
+        // Create NSHTTPCookie for _ncc
+        const cookieProperties = NSMutableDictionary.alloc().init();
+        cookieProperties.setObjectForKey("_ncc", NSHTTPCookieName);
+        cookieProperties.setObjectForKey("0", NSHTTPCookieValue);
+        cookieProperties.setObjectForKey("signin.nianticlabs.com", NSHTTPCookieDomain);
+        cookieProperties.setObjectForKey("/", NSHTTPCookiePath);
+
+        const nsCookie = NSHTTPCookie.cookieWithProperties(cookieProperties);
+
+        if (nsCookie) {
+          cookieStore.setCookieCompletionHandler(nsCookie, (error) => {
+            if (error) {
+              console.log('[iOS] Error setting auth cookie:', error.localizedDescription);
+            } else {
+              console.log('[iOS] Auth cookie _ncc set successfully for signin.nianticlabs.com');
+            }
+          });
+        }
+      } catch (error) {
+        console.log('[iOS] Could not set auth cookie:', error.message);
+      }
+
+      // Apply User Agent if needed
+      if (fakeUserAgent) {
+        const customUserAgent = getDesktopUserAgent();
+        webview_ios.customUserAgent = customUserAgent;
+
+        return {
+          originalUserAgent: webview_ios.customUserAgent,
+          finalUserAgent: customUserAgent
+        };
+      }
+    }
+
+    return {
+      originalUserAgent: null,
+      finalUserAgent: null
     };
   }
   return null;
